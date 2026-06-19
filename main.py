ed4de94fb07a6a936a4e78893c1278ebe78500aa
José Carlos <josecarlosnascimentosantos007@gmail.com>
2026-06-18 21:13:10 -0300

    Protótipo do simulador de resultados

--- main.py
+++ main.py
@@ -198,6 +198,28 @@ def usuario_page(request: Request, usuario: Optional[Usuario] = Depends(obter_us
         context={"nome": usuario.nome},
     )
 
+# -- SIMULADOR DE CAMPEONATOS
+
+@app.get("/simulador-page", response_class=HTMLResponse)
+def simulador_page(request: Request, usuario: Optional[Usuario] = Depends(obter_usuario_logado)):
+    return templates.TemplateResponse(
+        request=request,
+        name="simulador.html",
+        context={
+            "nome": usuario.nome if usuario else None
+        }
+    )
+
+@app.get("/api/simulador/confrontos-iniciais")
+def obter_confrontos_iniciais():
+    quartas_de_final = [
+        {"id_jogo": "q1", "time1": "NaVi", "time2": "FaZe"},
+        {"id_jogo": "q2", "time1": "Vitality", "time2": "G2 Esports"},
+        {"id_jogo": "q3", "time1": "FURIA", "time2": "MOUZ"},
+        {"id_jogo": "q4", "time1": "Team Spirit", "time2": "Virtus.pro"}
+    ]
+    return {"quartas": quartas_de_final}
+
 # --- ROTAS HTML de FORMULÁRIOS (mantidas, usando hash) ---
 
 @app.post("/cadastro")
